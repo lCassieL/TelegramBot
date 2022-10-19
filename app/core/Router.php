87554d189128f9d24a3af3a2b7fc8f5110a3b218
lCassieL <denisandwork@gmail.com>
2022-10-20 00:02:52 +0300

    see trello api 4

--- app/core/Router.php
+++ app/core/Router.php
@@ -5,7 +5,7 @@ class Router {
     static $TRELLO_KEY = '593fd8ef4b57c8d736fca2d220352a96';
     static function init() {
         $routes = explode('/', $_SERVER['REQUEST_URI']);
-        $param = 'telegram';
+        $param = '';
         if(!empty($routes[1])) {
             $param = strtolower($routes[1]);
         }
@@ -15,8 +15,8 @@ class Router {
                 $data = json_decode(file_get_contents('php://input'), TRUE);
                 $send_data = [
                     'method' => 'sendMessage',
-                    'text' => $test,
-                    'chat_id' => $data['chat']['id']
+                    'text' => 'dd '.$test,
+                    'chat_id' => '398498577'
                 ];
                 $res = Router::sendTelegram($send_data['method'], $send_data);
                 break;
@@ -29,18 +29,11 @@ class Router {
                     case 'старт':
                     case 'start':
                     case '/start':
-                        /*include 'app/models/ApiModel.php';
+                        include 'app/models/ApiModel.php';
                         include 'app/controllers/ApiController.php';
                         $controller = new ApiController();
                         $controller->action_start($data);
-                        break;*/
-                        $send_data = [
-                            'method' => 'sendMessage',
-                            'text' => $data['chat']['id'],
-                            'chat_id' => $data['chat']['id']
-                             ];
-                             $res = Router::sendTelegram($send_data['method'], $send_data);
-                             break;
+                        break;
                     default:
                         /*$send_data = [
                         'method' => 'sendMessage',
@@ -51,17 +44,6 @@ class Router {
                          break;*/
                 }
                 break;
-            default:
-            $data = json_decode(file_get_contents('php://input'), TRUE);
-            $data = $data['callback_query'] ? $data['callback_query'] : $data['message'];
-            $message = strtolower($data['text'] ? $data['text'] : $data['data']);
-            $send_data = [
-                'method' => 'sendMessage',
-                'text' => $data['chat']['id'],
-                'chat_id' => $data['chat']['id']
-                 ];
-                 $res = Router::sendTelegram($send_data['method'], $send_data);
-
         }
     }
 
