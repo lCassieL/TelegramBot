127884d3f0dab4b325fc004c30bd0912161bf3f8
lCassieL <denisandwork@gmail.com>
2022-10-19 15:47:39 +0300

    str to lowercase

--- app/core/Router.php
+++ app/core/Router.php
@@ -5,9 +5,11 @@ class Router {
         $test = file_get_contents('php://input');
         $data = json_decode(file_get_contents('php://input'), TRUE);
         $data = $data['callback_query'] ? $data['callback_query'] : $data['message'];
-        //$message = mb_strtolower(($data['text'] ? $data['text'] : $data['data']), 'utf-8');
-        $message = $data['text'] ? $data['text'] : $data['data'];
+        $message = strtolower(($data['text'] ? $data['text'] : $data['data']), 'utf-8');
         switch($message) {
+            case '/старт':
+            case 'старт':
+            case 'start':
             case '/start':
                 include 'app/models/ApiModel.php';
                 include 'app/controllers/ApiController.php';
@@ -23,36 +25,6 @@ class Router {
                 $res = Router::sendTelegram($send_data['method'], $send_data);
                 break;
         }
-        /*$controller_name = 'main';
-        $action_name = 'products';
-        $routes = explode('/', $_SERVER['REQUEST_URI']);
-        if(!empty($routes[1])) {
-            $controller_name = strtolower($routes[1]);
-        }
-        if(!empty($routes[2])) {
-            $action_name = strtolower($routes[2]);
-        }
-
-        $controller_name = ucfirst($controller_name);
-        $controller_class = $controller_name.'Controller';
-        $model_class = $controller_name.'Model';
-        $action = 'action_'.$action_name;
-        $model_path = 'app/models/'.$model_class.'.php';
-        if(file_exists($model_path)) {
-            include $model_path;
-        }
-        $controller_path = 'app/controllers/'.$controller_class.'.php';
-        if(file_exists($controller_path)) {
-            include $controller_path;
-        } else {
-            self::ErrorPage404();
-        }
-        $controller = new $controller_class;
-        if(method_exists($controller, $action)) {
-            empty($routes[3]) ? $controller->$action() : $controller->$action($routes[3]);
-        } else {
-            self::ErrorPage404();
-        }*/
     }
 
     public static function sendTelegram($method, $data, $headers = []) {
