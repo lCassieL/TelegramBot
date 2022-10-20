3cb612e2b279071c36a27f622bd7dcb6c78a1db4
lCassieL <denisandwork@gmail.com>
2022-10-20 08:58:27 +0300

    ready build

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -4,6 +4,5 @@ include_once 'core/Controller.php';
 include_once 'core/View.php';
 include_once 'core/Model.php';
 include_once 'core/Router.php';
-include_once 'helpers/GenerateHeader.php';
 session_start();
 router::init();
