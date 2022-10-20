0ac7f7e3d3a0179b5db8e758e3c47e1180366298
lCassieL <denisandwork@gmail.com>
2022-10-20 16:20:28 +0300

    fix model

--- app/models/ApiModel.php
+++ app/models/ApiModel.php
@@ -2,11 +2,11 @@
 class ApiModel extends Model {
     public function saveUser($data) {
         if($this->db->connect_errno === 0) {
-            $telegram_id = (int)$data['from']['id'];
+            $telegram_id = $data['from']['id'];
             $first_name = $data['from']['first_name'];
             $last_name = $data['from']['last_name'];
-            $first_name = mysqli_real_escape_string($this->db, $first_name);
-            $last_name = mysqli_real_escape_string($this->db, $last_name);
+            //$first_name = mysqli_real_escape_string($this->db, $first_name);
+            //$last_name = mysqli_real_escape_string($this->db, $last_name);
             $query="INSERT INTO users(telegram_id, first_name, last_name) VALUES('$telegram_id', '$first_name','$last_name');";
             $this->db->query($query);
             return $this->db->insert_id;
@@ -15,7 +15,7 @@ class ApiModel extends Model {
 
     public function getUser($id) {
         if($this->db->connect_errno === 0) {
-            $query = "SELECT * FROM users WHERE telegram_id=".(int)$id;
+            $query = "SELECT * FROM users WHERE telegram_id=".$id;
             $res = $this->db->query($query);
             if($res) {
                 return $res->fetch_all(MYSQLI_ASSOC);
@@ -39,8 +39,8 @@ class ApiModel extends Model {
 
     public function connectToTrelloAcc($data, $trello_id) {
         if($this->db->connect_errno === 0) {
-            $telegram_id = (int)$data['from']['id'];
-            $trello_id = mysqli_real_escape_string($this->db, $trello_id);
+            $telegram_id = $data['from']['id'];
+            //$trello_id = mysqli_real_escape_string($this->db, $trello_id);
             $query = "UPDATE users SET trello_id='$trello_id' WHERE telegram_id=".$telegram_id;
             $this->db->query($query);
         }
